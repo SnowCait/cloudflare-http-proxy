a1604e697826900a474392d8c2437eb2a3bf3a95
SnowCait <SnowCait@users.noreply.github.com>
2025-01-18 14:55:54 +0900

    Origin

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,25 @@ import { Hono } from "hono";
 import { cache } from "hono/cache";
 import { cors } from "hono/cors";
 
-const app = new Hono();
+type Env = {
+  Bindings: {
+    CORS_ORIGIN: string;
+  };
+};
+
+const app = new Hono<Env>();
 
 app.on(
   ["OPTIONS", "HEAD", "GET"],
   "/",
   cache({ cacheName: "default" }),
-  cors({
-    origin: ["https://nostter.app", "http://localhost:5173"],
-    allowMethods: ["HEAD", "GET"],
-  }),
+  async (c, next) => {
+    const middleware = cors({
+      origin: c.env.CORS_ORIGIN ?? "*",
+      allowMethods: ["HEAD", "GET"],
+    });
+    return middleware(c, next);
+  },
   (c) => {
     const url = c.req.query("url");
     if (url === undefined) {
